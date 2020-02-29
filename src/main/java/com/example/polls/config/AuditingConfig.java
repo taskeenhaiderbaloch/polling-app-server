171d91181aee11e9c742535a654dca2a136bc87d
taskeenhaiderbaloch <taskeenhaider7@gmail.com>
2020-02-29 21:39:12 +0500

    first commit

---
+++ src/main/java/com/example/polls/config/AuditingConfig.java
@@ -0,0 +1,23 @@
+package com.example.polls.config;
+
+import com.example.polls.security.UserPrincipal;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+    @Bean
+    public AuditorAware<Long> auditorProvider() {
+        return new SpringSecurityAuditAwareImpl();
+    }
+}
+
